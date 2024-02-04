7087086bda5aaffe0d3c56a5831d0302412336d7
Alok Sahu <tusharsahu2427@gmail.com>
2024-02-04 15:57:30 +0530

    first-commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,18 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import '@/styles/_newcopyfile.css';
+import '@/styles/animate.css'
+import '@/styles/blogdetails.css'
+import '@/styles/bootstrap.css'
+import '@/styles/editHome.css'
+// import '@/styles/bootstrap.css.map'
+import '@/styles/blogdetails.css'
+import '@/styles/owl.carousel.css'
+import '@/styles/owl.theme.default.css'
+// import '@/styles/style_1.css.map'
+import '@/styles/style_1.css'
+import '@/styles/media_query.css'
+
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
